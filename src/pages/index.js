efcd14723b2c21c98dc71461a4dd6b86f37e2bc0
Sarwar Ahmad <eboysam1@gmail.com>
2025-08-05 23:19:16 +0530

    v2

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import Home from "./HomePage";
+import About from "./AboutPage";
+import AuthWrapper from "./AuthWrapper";
+import Cart from "./CartPage";
+import Error from "./ErrorPage";
+import Checkout from "./CheckoutPage";
+import PrivateRoute from "./PrivateRoute";
+import Product from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+
+export {
+  Home,
+  About,
+  AuthWrapper,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+  Product,
+  SingleProduct,
+};
